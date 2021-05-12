0d57bc8f264696f53df9fa279b4b582adcb915e4
Emma Paris <eparis@atlassia>
2021-05-12 22:24:25 +0530

    Added:Day08 PracticeProblems

---
+++ Day08/birthday.sh
@@ -0,0 +1,16 @@
+declare -A birthDay
+echo "${birthDay[@]}"
+
+#VARIABLE
+index=1
+while [ $index -le 50 ]
+do
+        random=$((RANDOM%12+1))
+        birthDay[$random]=$((birthDay[$random]+1))
+                ((index++))
+done
+
+#PRINT
+echo "Individuals Having Birthdays in the Same Month."
+echo "${birthDay[@]}"
+
